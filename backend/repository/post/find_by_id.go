59b3eac5b871c1d62d5dd4baf9bf12754a76df4e
noueii <n0wayyy18@gmail.com>
2026-05-04 17:35:46 +0900

    feat: r2b

--- backend/repository/post/find_by_id.go
+++ backend/repository/post/find_by_id.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 
-	. "github.com/go-jet/jet/v2/postgres"
+	jetpostgres "github.com/go-jet/jet/v2/postgres"
 
 	"github.com/noueii/no-frame-works/db/no_frame_works/public/model"
 	"github.com/noueii/no-frame-works/db/no_frame_works/public/table"
@@ -14,9 +14,9 @@ import (
 )
 
 func (r *PostgresPostRepository) FindByID(ctx context.Context, id string) (*domain.Post, error) {
-	stmt := SELECT(table.Post.AllColumns).
+	stmt := jetpostgres.SELECT(table.Post.AllColumns).
 		FROM(table.Post).
-		WHERE(table.Post.ID.EQ(String(id)))
+		WHERE(table.Post.ID.EQ(jetpostgres.String(id)))
 
 	var dest model.Post
 	err := stmt.QueryContext(ctx, r.db, &dest)
